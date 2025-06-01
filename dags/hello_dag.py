1edeb3f8cc9dbfddbdc51ffa55f4fd47ab0fa14e
Mahavir Singh <msingh@Msinghs-Macbook.local>
2025-06-01 19:41:07 +0530

    first commit

---
+++ dags/hello_dag.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Hello from a Kubernetes Pod!")
+
+with DAG("hello_kubernetes",
+         start_date=datetime(2024, 1, 1),
+         schedule_interval=None,
+         catchup=False) as dag:
+    PythonOperator(
+        task_id="hello_task",
+        python_callable=hello
+    )
