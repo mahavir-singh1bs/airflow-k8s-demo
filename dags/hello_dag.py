d9ad33d66a673593989501dae10e742fa40bfe3d
Mahavir Singh <msingh@Msinghs-Macbook.local>
2025-06-01 20:03:55 +0530

    added wait to the python function

--- dags/hello_dag.py
+++ dags/hello_dag.py
@@ -1,9 +1,14 @@
+import time
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def hello():
     print("Hello from a Kubernetes Pod!")
+    time.sleep(10)
+    print("Again, hello from a Kubernetes Pod!")
+
+
 
 with DAG("hello_kubernetes",
          start_date=datetime(2024, 1, 1),
